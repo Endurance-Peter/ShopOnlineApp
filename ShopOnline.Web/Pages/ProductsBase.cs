f91495078231be9cd5e65cb51a9174b973a39be3
Endurance-Peter <endurance.peter@cyphercrescent.com>
2022-11-29 11:27:01 +0100

    added css styling for products

--- ShopOnline.Web/Pages/ProductsBase.cs
+++ ShopOnline.Web/Pages/ProductsBase.cs
@@ -15,5 +15,20 @@ namespace ShopOnline.Web.Pages
         {
             Products = await ProductService.GetProducts();
         }
+
+        protected IOrderedEnumerable<IGrouping<int,ProductDto>> GetGroupedProductsCategory()
+        {
+            return from product in Products
+                   group product by product.CategoryId into prodByCatGroup
+                   orderby prodByCatGroup.Key
+                   select prodByCatGroup;
+        }
+
+        protected string GetCategoryName(IGrouping<int, ProductDto> groupedProductDtos)
+        {
+            var result = groupedProductDtos.FirstOrDefault(pg => pg.CategoryId == groupedProductDtos.Key).CategoryName;
+            
+            return result;
+        }
     }
 }
